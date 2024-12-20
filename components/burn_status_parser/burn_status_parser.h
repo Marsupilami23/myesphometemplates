9b85ee325ba7041a71a81bdad96f1d64b0b16243
Marsupilami23 <marsupilami23@gmail.com>
2024-12-19 17:53:31 -0800

    Update burn_status_parser.h

--- components/burn_status_parser/burn_status_parser.h
+++ components/burn_status_parser/burn_status_parser.h
@@ -1,9 +1,26 @@
-#ifndef BURN_STATUS_PARSER_H
-#define BURN_STATUS_PARSER_H
+#ifndef BURN_FORECAST_H
+#define BURN_FORECAST_H
 
-#include <string>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/http_request/http_request.h"
 
-std::string extract_json_string(const std::string& body);
-bool parse_json(const std::string& json_string, int& burn_status);
+namespace esphome {
+namespace burn_forecast {
+
+class BurnForecast : public Component, public sensor::Sensor {
+ public:
+  void set_url(const std::string &url) { this->url_ = url; }
+
+  void setup() override;
+  void update() override;
+
+ protected:
+  std::string url_;
+  http_request::HTTPClient *http_client_{nullptr};
+};
+
+}  // namespace burn_forecast
+}  // namespace esphome
 
 #endif
