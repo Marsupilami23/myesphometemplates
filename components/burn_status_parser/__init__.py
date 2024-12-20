a892afbbbc18ce2676d1b60658ac179c121fc6fd
Marsupilami23 <marsupilami23@gmail.com>
2024-12-19 19:13:10 -0800

    Update __init__.py

--- components/burn_status_parser/__init__.py
+++ components/burn_status_parser/__init__.py
@@ -12,16 +12,17 @@ DEPENDENCIES = ["http_request"]
 AUTO_LOAD = ["json"]
 CODEOWNERS = ["Marsupilami23"]  # Replace with your GitHub username
 
+CONF_URL = "url"
+
 burn_forecast_ns = cg.esphome_ns.namespace("burn_forecast")
 BurnForecast = burn_forecast_ns.class_("BurnForecast", cg.Component, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BurnForecast),
-    cv.Required("url"): cv.url,
+    cv.Required(CONF_URL): cv.url,
 }).extend(cv.COMPONENT_SCHEMA).extend(sensor.SENSOR_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.add(var.set_url(config["url"]))
+    await cg.add(var.set_url(config[CONF_URL]))
     await sensor.register_sensor(var, config)
-    cg.add_global(burn_forecast_ns.using)
