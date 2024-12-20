f044f25cf7dbe4d8b27bff68f9c1be1d855dbb5f
Marsupilami23 <marsupilami23@gmail.com>
2024-12-19 19:38:19 -0800

    Update __init__.py

--- components/burn_forecast/__init__.py
+++ components/burn_forecast/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
-from esphome.core import const
 
 DEPENDENCIES = ["http_request"]
 AUTO_LOAD = ["json"]
